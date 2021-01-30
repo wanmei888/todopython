2b58e1d052ecf9c2429f4b62772ce1d93e8f2b7e
Aigerim <aikawanmei@gmail.com>
2021-01-31 00:32:00 +0600

    added third test page and url

--- main/views.py
+++ main/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render, HttpResponse
 def homepage(request):
     return HttpResponse("Hello world!")
 
+
 def test(request):
-    return render(request, "test.html") 
+    return render(request, "test.html")
+
 
 def second(request):
     return HttpResponse("test 2 page")
+
+
+def third(request):
+    return HttpResponse("This is page test3")
